38b2846df230090ce4ce79914612533999280c87
Aman Thukral <thukral618@gmail.com>
2024-05-07 14:32:56 +0530

    Adding basic implementation of useMemo hook

--- useMemoPolyfill/src/App.jsx
+++ useMemoPolyfill/src/App.jsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(100);
 
-  return <></>;
+  const squaredValue = () => {
+    console.log("Expensive calculation");
+    return counter * counter;
+  };
+
+  const memoizedSquaredValue = useMemo(squaredValue, [counter]);
+
+  return (
+    <>
+      <h2>Counter: {counter}</h2>
+      <h2>SquaredCouunter: {memoizedSquaredValue}</h2>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <h2>Counter2: {counter2}</h2>
+      <button onClick={() => setCounter2(counter2 - 1)}>Decrement</button>
+    </>
+  );
 }
 
 export default App;
